d689496781d1e8b5cd58a9f185f7eb78cf0b8a47
Alejandro Romero <alejo8591@gmail.com>
2012-05-22 13:53:34 -0500

    update directory data facturas

--- query/bulk_facturas.sql
+++ query/bulk_facturas.sql
@@ -1,4 +1,4 @@
-LOAD DATA LOCAL INFILE 'C:/Users/usuario/Documents/db_final/data/facturas.csv'
+LOAD DATA LOCAL INFILE 'C:/Documents and Settings/xp/Mis documentos/GitHub/dblab/data/facturas.csv'
 INTO TABLE facturas
 FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n';
