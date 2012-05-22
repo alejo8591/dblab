c1a1dd06176781ef0e9c25cf3d1a8cfd7e64fc4c
Alejandro Romero <alejo8591gmail.com>
2012-05-22 00:10:37 -0500

    update documentation trigger

--- query/trigger_table_saldofactura.sql
+++ query/trigger_table_saldofactura.sql
@@ -5,5 +5,8 @@ USE `ProyectoFinal`$$
 
 CREATE TRIGGER `saldo` BEFORE INSERT ON `facturas`
        FOR EACH ROW BEGIN
+       -- Instertando datos en la tabla `saldofactura` con respecto al 
+       -- campo noFactura y su valor de factura vrFactura para modificar este campo y no 
+       -- el original de la factura y lo mismo en pagos
         INSERT INTO saldofactura SET idFactura = NEW.noFactura, vrSaldo = NEW.vrFactura;
      END$$
