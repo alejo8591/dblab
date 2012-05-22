586a3e5c8cca5dce5935e86adb538777dce2d923
Alejandro Romero <alejo8591@gmail.com>
2012-05-22 13:45:25 -0500

    update push data into clientes table

--- query/bulk_clientes.sql
+++ query/bulk_clientes.sql
@@ -1,4 +1,4 @@
-LOAD DATA LOCAL INFILE 'C:/Users/usuario/Documents/db_final/data/clientes.csv'
+LOAD DATA LOCAL INFILE 'C:/Documents and Settings/xp/Mis documentos/GitHub/dblab/data/clientes.csv'
 INTO TABLE clientes
 FIELDS TERMINATED BY ';'
 LINES TERMINATED BY '\n';
